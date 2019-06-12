f6061ccb4b252671dffaa49b2f9d4c20a949cdd9
Abhishek Bains <abains8263@gmail.com>
2019-06-12 12:08:27 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,52 +1,39 @@
 const express = require('express');
-const logger = require('morgan');
-const users = require('./routes/api/user');
-const notes = require('./routes/api/note');
 const bodyParser = require('body-parser');
-const connectDB = require('./config/db'); //database configuration
-var jwt = require('jsonwebtoken');
+
+// create express app
 const app = express();
-app.set('secretKey', 'secret'); // jwt secret token
-// connection to mongodb
-connectDB();
-app.use(logger('dev'));
-app.use(bodyParser.urlencoded({extended: false}));
-app.get('/', function(req, res){
-res.json({"Project" : "Sticky notes using Node & React"});
-});
-// public route
-app.use('/users', users);
-app.use('/api/notes',notes);
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
 
-function validateUser(req, res, next) {
-  jwt.verify(req.headers['x-access-token'], req.app.get('secretKey'), function(err, decoded) {
-    if (err) {
-      res.json({status:"error", message: err.message, data:null});
-    }else{
-      // add user id to request
-      req.body.userId = decoded.id;
-      next();
-    }
-  });
-  
-}
-// express doesn't consider not found 404 as an error so we need to handle 404 explicitly
-// handle 404 error
-app.use(function(req, res, next) {
- let err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+// parse application/json
+app.use(bodyParser.json())
+
+// Configuring the database
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+// Connecting to the database
+mongoose.connect(dbConfig.url, {
+	useNewUrlParser: true
+}).then(() => {
+    console.log("Successfully connected to the database");    
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
 });
-// handle errors
-app.use(function(err, req, res, next) {
- console.log(err);
- 
-  if(err.status === 404)
-   res.status(404).json({message: "Not found"});
-  else 
-    res.status(500).json({message: "Something looks wrong :( !!!"});
+
+// define a simple route
+app.get('/', (req, res) => {
+    res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
 });
-app.listen(3000, function(){
- console.log('Node server listening on port 3000');
+
+require('./app/routes/note.routes.js')(app);
+
+// listen for requests
+app.listen(5000, () => {
+    console.log("Server is listening on port 5000");
 });
